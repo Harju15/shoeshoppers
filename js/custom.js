702108fa8b98b456192d8ca8a4a60df688fdadfc
Harju15 <92333865+Harju15@users.noreply.github.com>
2022-03-08 14:10:21 -0600

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
 //checkout page//
+
 $('form').card({
     container: '.card-wrapper',
     width: 280,
@@ -7,3 +8,18 @@ $('form').card({
         nameInput: 'input[name="first-name"], input[name="last-name"]'
     }
 });
+
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+const context = new AudioContext();
+
+navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+  const microphone = context.createMediaStreamSource(stream);
+  const filter = context.createBiquadFilter();
+  // microphone -> filter -> destination
+  microphone.connect(filter);
+  filter.connect(context.destination);
+});
+
+//new try for camera//
+
